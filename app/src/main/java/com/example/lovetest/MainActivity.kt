2ef37b26fa2562b8f0b91e1e85557dc3af9b004a
tjswhdrbs14 <tjswhdrbs14@naver.com>
2022-03-07 23:37:29 +0900

    Change kotlin-extensions to ViewBinding (Not Finished)
    TODO
    - Find way NavController on ViewBinding

--- app/src/main/java/com/example/lovetest/MainActivity.kt
+++ app/src/main/java/com/example/lovetest/MainActivity.kt
@@ -3,17 +3,20 @@ package com.example.lovetest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
-import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.navigation.findNavController
+import com.example.lovetest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        navController = nav_host_fragment.findNavController()
+        navController = findNavController(R.id.nav_host_fragment)
+
     }
 }
