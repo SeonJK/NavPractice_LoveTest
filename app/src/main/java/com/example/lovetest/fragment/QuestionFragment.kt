008038c576153007345f2cd1c0e1364ac85b9842
SeonJK <tjswhdrbs14@naver.com>
2022-03-17 00:03:40 +0900

    Change kotlin-extensions to ViewBinding Realize BackPressed on Navigation by PopUpTo
    TODO
    - Find way NavController on ViewBinding

--- app/src/main/java/com/example/lovetest/fragment/QuestionFragment.kt
+++ app/src/main/java/com/example/lovetest/fragment/QuestionFragment.kt
@@ -9,6 +9,7 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.lovetest.R
 import com.example.lovetest.databinding.FragmentMainBinding
+import com.example.lovetest.databinding.FragmentQuestionBinding
 
 //// TODO: Rename parameter arguments, choose names that match
 //// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,7 +24,7 @@ import com.example.lovetest.databinding.FragmentMainBinding
 class QuestionFragment : Fragment(), View.OnClickListener {
 
     lateinit var navController: NavController
-    lateinit var binding: FragmentMainBinding
+    lateinit var binding: FragmentQuestionBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,7 +32,7 @@ class QuestionFragment : Fragment(), View.OnClickListener {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentMainBinding.inflate(inflater, container, false)
+        binding = FragmentQuestionBinding.inflate(inflater, container, false)
 
         return binding.root
     }
@@ -40,14 +41,16 @@ class QuestionFragment : Fragment(), View.OnClickListener {
         super.onViewCreated(view, savedInstanceState)
 
         navController = Navigation.findNavController(view)
-        binding.btnNext.setOnClickListener { this }
+        binding.btnNext.setOnClickListener {
+            navController.navigate(R.id.action_questionFragment_to_selectionFragment)
+        }
     }
 
     override fun onClick(v: View?) {
-        when(v?.id){
-            R.id.btn_next -> {
-                navController.navigate(R.id.action_questionFragment_to_selectionFragment)
-            }
-        }
+//        when(){
+//              binding.btnNext -> {
+//                navController.navigate(R.id.action_questionFragment_to_selectionFragment)
+//            }
+//        }
     }
 }
--- app/src/main/java/com/example/lovetest/fragment/SelectionFragment.kt
+++ app/src/main/java/com/example/lovetest/fragment/SelectionFragment.kt
@@ -1,92 +1,67 @@
 package com.example.lovetest.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.TextUtils.replace
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.lovetest.R
-import kotlinx.android.synthetic.main.fragment_selection.*
+import com.example.lovetest.databinding.FragmentSelectionBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//private const val ARG_PARAM1 = "param1"
-//private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
  * Use the [SelectionFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SelectionFragment : Fragment(), View.OnClickListener {
-    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-//    }
+class SelectionFragment : Fragment() {
 
     lateinit var navController: NavController
+    lateinit var binding: FragmentSelectionBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_selection, container, false)
+        binding = FragmentSelectionBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         navController = Navigation.findNavController(view)
-        option_1.setOnClickListener { this }
-        option_2.setOnClickListener { this }
-        option_3.setOnClickListener { this }
-        option_4.setOnClickListener { this }
-
+        binding.option1.setOnClickListener { onButtonClicked() }
+        binding.option2.setOnClickListener { onButtonClicked() }
+        binding.option3.setOnClickListener { onButtonClicked() }
+        binding.option4.setOnClickListener { onButtonClicked() }
+        binding.btnBack.setOnClickListener { navController.navigate(R.id.action_selectionFragment_pop) }
     }
 
-    override fun onClick(v: View?) {
-        when(v?.id){
-            R.id.option_1 -> {
-                navController.navigate(R.id.action_selectionFragment_to_resultFragment)
-            }
-            R.id.option_2 -> {
-                navController.navigate(R.id.action_selectionFragment_to_resultFragment)
-            }
-            R.id.option_3 -> {
-                navController.navigate(R.id.action_selectionFragment_to_resultFragment)
-            }
-            R.id.option_4 -> {
-                navController.navigate(R.id.action_selectionFragment_to_resultFragment)
-            }
-        }
-    }
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment SelectionFragment.
-//         */
-//        // TODO: Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            SelectionFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
+//    override fun onClick(v: View?) {
+//        when(v?.id){
+//            R.id.option_1 -> {
+//                navController.navigate(R.id.action_selectionFragment_to_resultFragment)
 //            }
+//            R.id.option_2 -> {
+//                navController.navigate(R.id.action_selectionFragment_to_resultFragment)
+//            }
+//            R.id.option_3 -> {
+//                navController.navigate(R.id.action_selectionFragment_to_resultFragment)
+//            }
+//            R.id.option_4 -> {
+//                navController.navigate(R.id.action_selectionFragment_to_resultFragment)
+//            }
+//        }
 //    }
+
+    private fun onButtonClicked() {
+        navController.navigate(R.id.action_selectionFragment_to_resultFragment)
+    }
 }
+
